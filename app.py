25104d5f208318ee7dc5fb13d110c5f8ae5a0ac9
OlivierB <olivier.death@gmail.com>
2019-09-30 20:49:08 +0200

    add venv condition and git condition and create

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 import os
 import venv
 import subprocess
+import git
+import time
+import shutil
 
 
-
-def new_projet(projet_name):
+def new_projet(projet_name,venv=True,git=True):
     global_folder_path = "/home/kilann/Documents/python_dev"
     #CUR_DIR = os.path.dirname(global_folder_path)
     PROJET_PATH = os.path.join(global_folder_path, projet_name)
@@ -23,14 +25,31 @@ def new_projet(projet_name):
         with open(APP_FILE, "a"):
             os.utime(APP_FILE, None)
 
-    if not os.path.exists(ENV_PATH):
-        venv.create(ENV_PATH,with_pip=True)
-
+    if venv == True:
+        if not os.path.exists(ENV_PATH): 
+            venv.create(ENV_PATH,with_pip=True)
+            
+    os.chdir(PROJET_PATH)
+    cmd = 'code .'
+    os.system(cmd)
+    time.sleep(2)        
         
-        os.chdir(PROJET_PATH)
-        cmd = 'code .'
-        os.system(cmd)
+    if git == True:
+        gitignore_origine = "/home/kilann/Documents/Dev_Learning/New_Project_Creator/.gitignore_default"
+        gitignore_dest = os.path.join(PROJET_PATH, '.gitignore')
+        if not os.path.exists(gitignore_dest):
+            with open(gitignore_dest, "a"):
+                os.utime(APP_FILE, None)
+
+        shutil.copy2(gitignore_origine, gitignore_dest)
+        subprocess.Popen("git init", stdout=subprocess.PIPE, shell=True)
+        os.chdir(APP_PATH)
+        time.sleep(1)
+        subprocess.Popen("git add app.py", stdout=subprocess.PIPE, shell=True)
+        time.sleep(1)
+        subprocess.Popen("git commit -m 'first commit'", stdout=subprocess.PIPE, shell=True)
+
        
 if __name__ == '__main__':
-    projet_name = input("nom du projet")
-    new_projet(projet_name)
+    projet_name = input("nom du projet: ")
+    new_projet(projet_name,venv=False)
